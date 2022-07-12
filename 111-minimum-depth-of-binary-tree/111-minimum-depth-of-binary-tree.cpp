5454dc7f57465f5603885df5f7f68c3e548ed758
SHIKHArajCHAUHAN <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-07-12 13:22:46 +0530

    Time: 371 ms (54.55%), Space: 144.9 MB (25.16%) - LeetHub

---
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+      if(root==NULL)  return 0;
+        if(!root->left&& !root->right) return 1;
+        if(!root->left) return minDepth(root->right)+1;
+        if(!root->right) return minDepth(root->left)+1;
+       int lh= minDepth(root->left);
+        int rh=minDepth(root->right);
+        return min(lh,rh)+1;
+    }
+};
