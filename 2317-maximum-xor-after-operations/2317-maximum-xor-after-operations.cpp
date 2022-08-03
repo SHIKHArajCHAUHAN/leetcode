de48338713f79486e8703ae7dc214e1f1c897afb
SHIKHArajCHAUHAN <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-08-03 17:14:40 +0530

    Time: 96 ms (97.09%), Space: 48.1 MB (49.18%) - LeetHub

---
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximumXOR(vector<int>& nums) {
+       int res=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            res=res|nums[i];
+        }
+        return res;
+    }
+};
