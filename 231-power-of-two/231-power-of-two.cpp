26e8c6d2d911f6e98030624acea0debe53a31a9a
SHIKHArajCHAUHAN <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-08-18 09:30:31 +0530

    Time: 6 ms (21.06%), Space: 5.9 MB (26.88%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -6,11 +6,18 @@ public:
            if(n==pow(2,x))
                return true;}
         return false;*/ 
-       // return n>0&& (n& n-1)==0;
+     /*  // return n>0&& (n& n-1)==0;
         long long r=1;
         while(r<n){
             r*=2;}
-        return r==n;
+        return r==n;*/
+       if(n==1) return true;
+        if(n==0) return false;
+        int r=n%2;
+        if(r!=0) return false;
+            return isPowerOfTwo(n/2);
+        
+    
         
     }
 };
