9f7363d3ec91254ce925b67822c34ad040e08e0d
SHIKHArajCHAUHAN <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-09-03 17:22:14 +0530

    Time: 6 ms (16.76%), Space: 6.7 MB (11.70%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countPoints(string rings) {
+      int count=0;
+        int n=rings.size();
+        map<int,set<char>>mp;
+            for(int i=1;i<n;i+=2)
+            {
+            mp[rings[i]-'0'].insert(rings[i-1]);
+            }
+        for(auto i:mp)
+        {
+           if(i.second.size()==3) count++; 
+        }
+        return count;
+    }
+};
