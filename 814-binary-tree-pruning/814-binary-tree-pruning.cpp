3485e7922f1e89c1c575d6b5fd8ecebfecb5a4ae
SHIKHArajCHAUHAN <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-09-06 15:43:37 +0530

    Time: 10 ms (6.47%), Space: 9.3 MB (5.18%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -12,20 +12,43 @@
 class Solution {
 public: TreeNode* fn(TreeNode *root)
 {
-    if(root==NULL) return NULL;
-   /* if(root->left==NULL&& root->right==NULL&& root->val==0)
-    {
-        free(root);
-    }*/
+   if(root==NULL) return NULL;
    root->left= fn(root->left);
-    root->right=fn(root->right);
-   if(root->left==NULL && root->right==NULL && root->val==0)
-    {
-        delete(root);
-       return NULL;
-    } 
+   root->right= fn(root->right);
+    if(root->right==NULL&&root->left==NULL&&root->val==0) 
+    {delete(root);
+     return NULL;}
+   // if(root->left!=NULL&&root->left->val==0) delete root->left;
     return root;
+    
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     if(root==NULL) return NULL;
+//    /* if(root->left==NULL&& root->right==NULL&& root->val==0)
+//     {
+//         free(root);
+//     }*/
+//    root->left= fn(root->left);
+//     root->right=fn(root->right);
+//    if(root->left==NULL && root->right==NULL && root->val==0)
+//     {
+//         delete(root);
+//        return NULL;
+//     } 
+//     return root;
+// }
     TreeNode* pruneTree(TreeNode* root) {
      return  fn(root);
         
