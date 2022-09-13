04b2c46000d641a81e6c4637e4f2d5b577fc3f85
SHIKHArajCHAUHAN <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-09-13 11:10:37 +0530

    Time: 3 ms (99.55%), Space: 9.5 MB (9.42%) - LeetHub

--- 883-projection-area-of-3d-shapes/883-projection-area-of-3d-shapes.cpp
+++ 883-projection-area-of-3d-shapes/883-projection-area-of-3d-shapes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int projectionArea(vector<vector<int>>& grid) {
-     int n = grid.size();
+    /* int n = grid.size();
         int x = n * n;
         for(int i = 0; i < n; i++){
             int y = 0, z = 0;
@@ -13,5 +13,17 @@ public:
             x += y + z;
         }
         return x;   
+    }*/
+         int res = 0, n = grid.size(), x, y;
+        for(int i = 0; i < n; i++) {
+            x = 0, y = 0;
+            for(int j = 0; j < n; j++) {
+                x = max(x, grid[i][j]);
+                y = max(y, grid[j][i]);
+                if(grid[i][j]) ++res;
+            }
+            res += x + y;
+        }
+        return res;
     }
 };
