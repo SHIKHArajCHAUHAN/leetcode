35a303424d301d19549e294e9f5788167123c509
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-09-22 15:29:44 +0530

    Time: 8 ms (45.53%), Space: 7 MB (98.84%) - LeetHub

---
+++ 541-reverse-string-ii/541-reverse-string-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+       
+        
+        for(int i = 0; i < s.length(); i += 2 * k)
+        {
+            if(i + k >= s.length())
+                reverse(s.begin() + i, s.end()); 
+            else
+                reverse(s.begin() + i, s.begin() + i + k);
+        }
+        
+        return s;
+    }
+};
