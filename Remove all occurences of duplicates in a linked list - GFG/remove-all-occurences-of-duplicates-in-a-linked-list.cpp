7b059c1c62fced4c1c1324c1e6b84675a9db44be
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-10-22 13:17:36 +0530

    Added solution - LeetHub

---
+++ Remove all occurences of duplicates in a linked list - GFG/remove-all-occurences-of-duplicates-in-a-linked-list.cpp
@@ -0,0 +1,113 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+// } Driver Code Ends
+//User function Template for C++
+
+/* Linked List node structure
+    
+struct Node {
+	int data;
+	struct Node *next;
+};
+
+*/
+
+class Solution
+{
+    public:
+    Node* removeAllDuplicates(struct Node* head)
+    {
+        //code here
+        map<int,int>mp;
+        Node*temp=head;
+       while(temp!=NULL)
+       {
+         mp[temp->data]++;
+         temp=temp->next;
+       }
+       Node *temp2=new Node(-1);
+       temp=temp2;
+       for(auto i:mp)
+       {
+           if(i.second==1)
+          {  temp->next=new Node(i.first);
+          temp=temp->next;
+          }
+       }
+       if(temp!=temp2)
+       {
+           temp2=temp2->next;
+       return temp2;
+       }
+       return NULL;
+    }
+};
+
+//{ Driver Code Starts.
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+
+
+// Driver program to test above functions
+int main()
+{
+	int T;
+	cin >> T;
+
+
+	while (T--)
+	{
+		int N;
+
+		cin >> N ;
+
+		Node *head = NULL;
+		Node *temp = head;
+
+		for (int i = 0; i < N; i++) {
+			int data;
+			cin >> data;
+			if (head == NULL)
+				head = temp = new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+
+		Solution ob;
+		head = ob.removeAllDuplicates(head);
+		printList(head);
+
+		cout << "\n";
+
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
