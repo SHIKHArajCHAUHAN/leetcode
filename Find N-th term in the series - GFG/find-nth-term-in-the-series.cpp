df215283b97779f22de48933e97495a8803dbac5
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-10-25 12:56:17 +0530

    Added solution - LeetHub

---
+++ Find N-th term in the series - GFG/find-nth-term-in-the-series.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int nthOfSeries(long long int n){
+        // code here
+          if(n==1)
+            return 9;
+        return nthOfSeries(n-1)+(16*n-8);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.nthOfSeries(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
