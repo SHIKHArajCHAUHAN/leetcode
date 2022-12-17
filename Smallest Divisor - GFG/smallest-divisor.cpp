51e9e0617ba7400bf454203191b585545e12d64b
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-12-17 12:58:35 +0530

    Added solution - LeetHub

--- Smallest Divisor - GFG/smallest-divisor.cpp
+++ Smallest Divisor - GFG/smallest-divisor.cpp
@@ -12,6 +12,33 @@ class Solution {
     int smallestDivisor(vector<int>& nums, int K) {
 
         // Write your code here.
+      /*   int low = 1, high = INT_MAX;
+        int ans = 0;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            long long int sum = 0;
+            for(int i=0;i<nums.size();i++)//to find sum
+            {
+                if(nums[i]%mid==0)
+                {
+                    sum += (nums[i]/mid);
+                }
+                else{
+                    sum += (nums[i]/mid)+1;
+                }
+            }
+            if(sum > K)
+            {
+                low = mid + 1;
+            }
+            else{
+                ans = mid;
+                high = mid - 1;
+            }
+        }
+        return ans;*/
          int low = 1, high = INT_MAX;
         int ans = 0;
 
