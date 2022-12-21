59e52b0ed6caf26680349e54c647a32079c66433
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-12-21 08:37:44 +0530

    Added solution - LeetHub

---
+++ Longest Consecutive 1's - GFG/longest-consecutive-1s.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*  Function to calculate the longest consecutive ones
+*   N: given input to calculate the longest consecutive ones
+*/
+class Solution
+{
+    public:
+    int maxConsecutiveOnes(int N)
+    {
+        // code here
+       int maxx=0;
+       int cnt=0;
+       while(N)
+       {
+           if((N&1)!=0) 
+           {cnt++;
+            N=N>>1;
+           }
+           else
+           {
+               maxx=max(maxx,cnt);
+               cnt=0;
+                N=N>>1;
+           }
+          
+           maxx=max(maxx,cnt);
+       }
+       return maxx;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main() {
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n;
+		cin>>n;//input n
+		Solution obj;
+		//calling maxConsecutiveOnes() function
+		cout<<obj.maxConsecutiveOnes(n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
