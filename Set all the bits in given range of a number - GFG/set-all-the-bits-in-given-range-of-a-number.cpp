f5a3a09e964917d212b7a284269fbb2a2fc36f74
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-12-28 11:19:12 +0530

    Added solution - LeetHub

---
+++ Set all the bits in given range of a number - GFG/set-all-the-bits-in-given-range-of-a-number.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setAllRangeBits(int N , int L , int R) {
+        // code here
+        for(int i=L-1;i<R;i++)
+        {
+            if((N&(1<<i))==0)
+            N=N|(1<<i);
+        }
+        return N;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,L,R;
+        
+        cin>>N>>L>>R;
+
+        Solution ob;
+        cout << ob.setAllRangeBits(N,L,R) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
