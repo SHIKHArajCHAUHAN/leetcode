e425767beeaaa4c58987d052cc8bc6865e5b6a0d
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2022-12-31 13:25:57 +0530

    Time: 0 ms (100.00%), Space: 6 MB (27.27%) - LeetHub

---
+++ 0672-bulb-switcher-ii/0672-bulb-switcher-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int flipLights(int n, int presses) {
+           if (presses==0) return 1;   
+        if (n==1)
+            if (presses>=1) return 2;
+                
+        if (n==2){
+            if (presses==1) return 3;
+            
+            if (presses>=2) return 4;
+        }
+        if (n>=3){
+            if (presses==1) return 4;
+            if (presses==2) return 7;
+            if (presses>=3) return 8;
+        }
+        
+        return 0;  
+    }
+};
