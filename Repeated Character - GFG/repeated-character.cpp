0e1fdef00b8ec270655f2a686d0de1b256b79986
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-01-07 23:19:06 +0530

    Added solution - LeetHub

---
+++ Repeated Character - GFG/repeated-character.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    char firstRep (string s)
+    {
+        //code here.
+        for(int i=0;i<s.length()-1;i++)
+        {
+            for(int j=i+1;j<s.length();j++)
+            {
+                if(s[i]==s[j]) return s[i];
+            }
+        }
+        return '#';
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        char res = ob.firstRep (s);
+        if (res == '#') cout << -1 << endl;
+        else cout << res << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
