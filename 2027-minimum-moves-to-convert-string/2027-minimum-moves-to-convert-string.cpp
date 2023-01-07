de2e6439a72d1598b3740f8f38b3ed1d5465b717
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-01-07 15:05:50 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (89.25%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+      int n=s.length();
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='X') 
+            {
+            cnt++;i+=2;}
+        
+            
+        }
+     
+        return cnt;
+    }
+};
