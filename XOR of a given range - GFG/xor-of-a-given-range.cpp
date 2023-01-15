7911b32cc6306c06e24efd52ced96b93a7d0faf8
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-01-15 19:17:49 +0530

    Added solution - LeetHub

---
+++ XOR of a given range - GFG/xor-of-a-given-range.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int getXor(vector<int>&nums, int a, int b){
+        // Code here
+        int xorr=nums[a];
+        for(int i=a+1;i<=b;i++)
+        {
+            xorr^=nums[i];
+        }
+        return xorr;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int n, a, b;
+        cin >> n >> a >> b;
+        vector<int>nums(n);
+        for(int i = 0; i < n; i++)cin >> nums[i];
+        if(a > b)swap(a, b);
+        Solution obj;
+        int ans = obj.getXor(nums, a, b);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
