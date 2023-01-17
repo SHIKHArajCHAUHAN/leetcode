98bec0dd8779ac0c47d921a2c590e98fbd00ff53
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-01-17 17:41:49 +0530

    Added solution - LeetHub

---
+++ Squares in a Matrix - GFG/squares-in-a-matrix.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInMatrix(int m, int n) {
+        // code here
+        if(n==0||m==0)return 0;
+        else return (long long)m*n+ squaresInMatrix(m-1,n-1);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int m,n;
+        
+        cin>>m>>n;
+
+        Solution ob;
+        cout << ob.squaresInMatrix(m,n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
