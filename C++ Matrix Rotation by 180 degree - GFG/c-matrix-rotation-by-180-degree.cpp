0cd636edddb3b80da1e592cd6a6bebb9980b869c
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-01-18 21:44:06 +0530

    Added solution - LeetHub

---
+++ C++ Matrix Rotation by 180 degree - GFG/c-matrix-rotation-by-180-degree.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    void rotate(vector<vector<int> >& matrix) {
+        // Code here
+        int n=matrix.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i;j++)
+            {
+                swap(matrix[i][j],matrix[n-1-j][n-1-i]);
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>> matrix(n, vector<int>(n,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrix[i][j];
+			}
+		}
+		Solution ob;
+		ob.rotate(matrix);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++)
+				cout << matrix[i][j] <<" ";
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
