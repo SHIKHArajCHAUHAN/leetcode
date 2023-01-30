31922f3be41478bd4331259467d83e6186535d82
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-01-30 09:08:57 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.21%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -5,7 +5,7 @@ public:
         if(n==1||n==2)return 1;
         vector<int>dp(n+1);
         dp[0]=0;
-        dp[1]=1;
+        dp[1]=1;                           //memoisation
         dp[2]=1;
     
         for(int i=3;i<=n;i++)
@@ -15,7 +15,7 @@ public:
         return dp[n];*/
             if(n==0) return 0;
         int a=0;
-        int b=1;
+        int b=1;                     //optimise code
         int c=1;
         for(int i=3;i<=n;i++)
         {
