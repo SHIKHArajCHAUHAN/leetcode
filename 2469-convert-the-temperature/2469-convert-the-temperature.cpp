b209b9d404119395b539e6dc7b6d3e7b4620ed31
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-02-01 19:59:39 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (63.66%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double k=celsius+273.15;
+        double f=celsius*1.80+32.00;
+        return {k,f};
+    }
+};
