37412486c4fad27499840f6626e4de1d32543d08
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-02-09 16:00:14 +0530

    Added solution - LeetHub

---
+++ Check if a number is power of another number - GFG/check-if-a-number-is-power-of-another-number.cpp
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int isPowerOfAnother(long long X, long long Y){
+        // code here 
+        return pow(X,int (log(Y)/log(X)))==Y;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long int X, Y;
+        cin >> X >> Y;
+        Solution ob;
+        cout << ob.isPowerOfAnother(X,Y) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
