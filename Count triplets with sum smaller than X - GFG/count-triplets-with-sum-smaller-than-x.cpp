403481408d6c64d738e3b544bb7f01ad03366c32
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-02-12 15:54:26 +0530

    Added solution - LeetHub

---
+++ Count triplets with sum smaller than X - GFG/count-triplets-with-sum-smaller-than-x.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	long long countTriplets(long long arr[], int n, long long sum)
+	{
+	    // Your code goes here
+	    sort(arr,arr+n);
+	    long long ans=0;
+	    for(int i=0;i<n-2;i++)
+	    {
+	       int s=i+1;int e=n-1;//two pter;
+	       while(s<e)
+	       {
+	           if(arr[i]+arr[e]+arr[s]<sum){ ans+=e-s; s++;}
+	           else e--;
+	           
+	       }
+	       
+	    }
+	    return ans;
+	}
+		 
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+  
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+    	long long sum;
+        cin>>n>>sum;
+        long long arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+	    
+
+       
+
+        Solution ob;
+        cout << ob.countTriplets(arr, n, sum) ;
+	   
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
