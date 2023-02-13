cdc3e441bfa64e47df910203af0a3dd93cff7702
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-02-13 15:55:52 +0530

    Added solution - LeetHub

---
+++ k sorted array - GFG/k-sorted-array.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    string isKSortedArray(int arr[], int n, int k)
+    {
+        map<int,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[arr[i]]=i;
+        }
+        sort(arr,arr+n);
+      for(int i=0;i<n;i++)
+      {
+          if(abs(mp[arr[i]]-i)>k)return "No";
+      }
+      return "Yes";
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,k;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    cin>>k;
+	    Solution ob;
+	cout <<ob.isKSortedArray(arr, n, k)<<endl;
+	}
+	return 0;	 
+}
+// } Driver Code Ends
