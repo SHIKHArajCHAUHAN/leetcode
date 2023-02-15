df41a4ea19e7f2f8bf48d033d1019ea79cfec12b
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-02-15 23:42:27 +0530

    Added solution - LeetHub

---
+++ Mean of range in array - GFG/mean-of-range-in-array.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+     vector<int> findMean(int arr[],int queries[],int n,int q)
+    {
+        // Complete the function
+           vector<int> ans;
+        int prefix[n];
+        prefix[0]=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            prefix[i]=prefix[i-1]+arr[i];
+        }
+        for(int i=0;i<q;i=i+2)
+        {
+            int sum=0;
+
+            int l=queries[i];
+
+            int r=queries[i+1];
+
+            if(l==0) sum=prefix[r];
+
+            else sum=prefix[r]-prefix[l-1];
+
+            int mean=sum/(r-l+1);
+
+            ans.push_back(mean);
+        }
+        return ans;
+    }
+
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,q;
+        cin>>n>>q;
+        int arr[n + 1],queries[2*q + 1];
+
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+
+        for(int i=0;i<2*q;i++){
+            cin>>queries[i];
+        }
+
+        vector<int> ans;
+        Solution ob;
+        ans = ob.findMean(arr,queries,n,2*q);
+
+        for(int i:ans)
+            cout<<i<<" ";
+        cout<<endl;
+
+    }
+    return 0;
+}
+
+// } Driver Code Ends
