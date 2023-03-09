98bd7aa96c9f60a6833a2b4bcc214878f5bc7dd7
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-03-10 01:15:31 +0530

    Time: 295 ms (16.09%), Space: 77.5 MB (11.40%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-       int n=arr.size();
-        int ans=0;
+//        int n=arr.size();
+//         int ans=0;
+//         map<int,int>mp;
+//         for(int i=0;i<arr.size();i++){
+//           mp[(k + arr[i] % k) % k]++;
+           
+//        }
+//        for(int i=0;i<arr.size();i++){
+//            int rem=arr[i]%k;
+//            if(rem==0){
+//               if(mp[rem]%2!=0) return false;
+//            }
+           
+//           else{
+//               int rem2=k-rem;
+//               if(mp[rem]!=mp[rem2])  return false;
+//            }
+//        }
+//         return true;
         map<int,int>mp;
-        for(int i=0;i<arr.size();i++){
-          mp[(k + arr[i] % k) % k]++;
-           
-       }
-       for(int i=0;i<arr.size();i++){
-           int rem=arr[i]%k;
-           if(rem==0){
-              if(mp[rem]%2!=0) return false;
-           }
-           
-          else{
-              int rem2=k-rem;
-              if(mp[rem]!=mp[rem2])  return false;
-           }
-       }
+        for(auto i:arr)
+        {
+           // mp[i%k]++; bss positve k liy hai
+            //negative casevk liy hme kuchh or h i krna prega 
+            mp[(i%k+k)%k]++;
+        }
+        if(mp[0]%2==1) return false;
+        for(int i=1;i<k;i++)
+        {
+            if(mp[i]!=mp[k-i])return false;
+        }
         return true;
     }
 };
