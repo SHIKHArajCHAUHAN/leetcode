207d5541dc58970f3833a04c39cdb313174a180d
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-03-12 22:47:36 +0530

    Added solution - LeetHub

--- Binary matrix having maximum number of 1s - GFG/binary-matrix-having-maximum-number-of-1s.cpp
+++ Binary matrix having maximum number of 1s - GFG/binary-matrix-having-maximum-number-of-1s.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     vector<int> findMaxRow(vector<vector<int>> mat, int N) {
         //code here
-        int index=0;
+         int index=0;
         int maxx=0;
         for(int i=0;i<N;i++)
         {
