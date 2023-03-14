b7475daa58de284c92820da2fc2082839c2a6d1a
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-03-14 18:01:26 +0530

    Added solution - LeetHub

---
+++ Count the number of possible triangles - GFG/count-the-number-of-possible-triangles.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to count the number of possible triangles.
+    int findNumberOfTriangles(int arr[], int n)
+    {
+        // code here
+         sort(arr,arr+n);
+        int cnt =0;
+        for(int i=n-1;i>=0;i--){
+            int start=0;
+            int end =i-1;
+            
+            while(start<end){
+                if(arr[start]+arr[end]>arr[i]){
+                    cnt = cnt+(end-start);
+                    end--;
+                }
+                else
+                start++;
+            }
+        }
+        return cnt;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.findNumberOfTriangles(arr,n) <<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
