e55d3159715fde129c6ed3d91a126682b7063f3a
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-03-31 15:04:25 +0530

    Time: 98 ms (80.19%), Space: 31.3 MB (57.82%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -3,21 +3,28 @@ public:
     int minSwaps(string s) {
        int extra=0;
         int maxx=0;
-        stack<char>st;
+//         stack<char>st;
+//         for(int i=0;i<s.length();i++)
+//         {
+//             if(s[i]=='[') st.push(s[i]);
+//             else 
+//             {
+//                 if(st.empty()==false&&s[i]==']')
+//                 {
+//                     st.pop();
+                    
+//                 }
+//                 else extra++;
+//             }
+        
+//         }
+//         return (extra+1)/2;
         for(int i=0;i<s.length();i++)
         {
-            if(s[i]=='[') st.push(s[i]);
-            else 
-            {
-                if(st.empty()==false&&s[i]==']')
-                {
-                    st.pop();
-                    
-                }
-                else extra++;
-            }
-        
+            if(s[i]=='[') extra-=1;
+            else extra+=1;
+            maxx=max(maxx,extra);
         }
-        return (extra+1)/2;
+        return (maxx+1)/2;
     }
 };
