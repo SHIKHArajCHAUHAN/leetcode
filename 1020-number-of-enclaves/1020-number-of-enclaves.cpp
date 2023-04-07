eb15449548ddf0c813b8a17a3308ccca19e5cd4b
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-04-07 11:11:54 +0530

    Time: 77 ms (66.79%), Space: 28.4 MB (68.59%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int dx[4]={-1,0,1,0};
+    int dy[4]={0,1,0,-1};
+    void dfs(int i,int j,vector<vector<int>>&grid,int n,int m)
+    {
+        grid[i][j]=0;
+        for(int k=0;k<4;k++)
+        {
+            int nr=i+dx[k];
+            int nc=j+dy[k];
+            if(nr>=0&&nr<n&&nc>=0&&nc<m&&grid[nr][nc]==1)
+                dfs(nr,nc,grid,n,m);
+        }
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(j*i==0||i==n-1||j==m-1)
+                {
+                    if(grid[i][j]==1)
+                        dfs(i,j,grid,n,m);
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                    if(grid[i][j]==1)
+    cnt++;
+            }
+        }
+        return cnt;
+        
+    }
+};
