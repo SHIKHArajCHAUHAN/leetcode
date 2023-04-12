ffac899959e04f9cc215095c217aedf38a6c406d
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-04-12 22:56:21 +0530

    Added solution - LeetHub

---
+++ Valid Substring - GFG/valid-substring.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for c++
+class Solution {
+  public:
+    int findMaxLen(string s) {
+        // code here
+        int n=s.length();
+        stack<int>st;               /////done by help
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(')
+            {
+                st.push(i);
+            }
+            else if (s[i]==')' && !st.empty())
+            {
+                s[i]='x';
+                s[st.top()]='x';
+                st.pop();
+            }
+        }
+
+        int maxx=0,cnt=0;
+
+        for(int i=n-1;i>=0;)
+        {
+            if(s[i]=='x')
+            {
+                cnt++;
+                while(i>=1 && s[i-1]=='x')
+                {
+                    cnt++;
+                    i--;
+                }
+                i--;
+            }
+            else 
+            {
+                i--;
+            }
+            
+            if(maxx<cnt)
+            maxx=cnt;
+
+            cnt=0;
+            
+        }
+        
+        return maxx;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S; 
+        Solution ob;
+        cout << ob.findMaxLen(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
