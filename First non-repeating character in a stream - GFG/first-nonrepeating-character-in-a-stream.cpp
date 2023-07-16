8c106bc60cd975ce4b0b507ebdff8a03246509db
Shikha Raj Chauhan <91404835+SHIKHArajCHAUHAN@users.noreply.github.com>
2023-07-17 00:41:42 +0530

    Added solution - LeetHub

--- First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
@@ -7,46 +7,30 @@ class Solution {
 	public:
 		string FirstNonRepeating(string A){
 		    // Code here
-		  /*  string ans="";
-    map<char,long>mp;
-     deque<char>dq;
-
-    for(long i=0;i<A.length();i++){
-          dq.push_back(A[i]);
-          mp[A[i]]++;
-         while(mp[dq.front()]>1)
-          dq.pop_front();
-           if(dq.empty())
-            ans+='#';
-            else
-           ans+=dq.front();
-    }
-     return ans;*/
-     vector<int>vis(26,0);
-     vector<char>v;
-     string ans="";
-     for(int i=0;i<A.length();i++)
-     {
-         if(!vis[A[i]-'a'])
-         {
-             v.push_back(A[i]);
-         }
-         vis[A[i]-'a']++;
-         int f=0;
-         int m=v.size();
-         for(int j=0;j<m;j++)
-         {
-             if(vis[v[j]-'a']==1)
-             {
-                 ans.push_back(v[j]);
-                 f=1;
-                 break;
-             }
-         }
-         if(f==0)ans.push_back('#');
-     }
-     return ans;
+		     string ans="";
+            set<char> st;
+            map<char,int> mp;
+            for(int i=0;i<A.length();i++){
+                if(mp[A[i]]==0){
+                    st.insert(A[i]);
+                }else{
+                    if(st.find(A[i])!=st.end()){
+                        st.erase(st.find(A[i]));
+                    }
+                }
+                if(mp[A[i]]==0)mp[A[i]]=i+1;
+                char c='#';int t=INT_MAX;
+                for(auto it:st){
+                    if(mp[it]<t){
+                        t=mp[it];
+                        c=it;
+                    }
+                }
+                ans+=c;
+            }
+            return ans;
 		}
+
 };
 
 //{ Driver Code Starts.
